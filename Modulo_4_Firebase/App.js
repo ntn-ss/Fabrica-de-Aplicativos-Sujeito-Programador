af905b33d5c0faf79a2b7eb37935b5cc616ec128
ntn-ss <91851915+ntn-ss@users.noreply.github.com>
2023-10-21 15:30:28 -0300

    Iniciando Módulo 4

---
+++ Modulo_4_Firebase/App.js
@@ -0,0 +1,20 @@
+import { StyleSheet, View } from 'react-native';
+import HelloWorld from './src/components/HelloWorld';
+
+export default function App() {
+
+  return (
+    <View style={styles.container}>
+      <HelloWorld />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
